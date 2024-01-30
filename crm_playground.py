318958b256a47eb9019038ca6b45362c2f076326
DebajyotiGoswami <goswamidiary@gmail.com>
2024-01-30 15:52:36 +0530

    create_folder() function created to create a folder where all output files will be stored

--- crm_playground.py
+++ crm_playground.py
@@ -15,9 +15,24 @@ def file_exists(filename):
         print("Filename {} does not exists . Create the file and try again".format(filename))
         exit(1)
 
+def create_folder(foldername):
+    '''
+    this function create a folder named "crm_files". Nothing else.
+    argument -- text
+    return -- None
+    '''
+    new_path = os.getcwd() + '//' + foldername
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+        print("{} folder Created".format(foldername))
+    else:
+        print("{} folder already exists".format(foldername))
+
 def main():
     filename = "APPLICATION_DETAILS.xlsx"
+    foldername = 'ALL_CRM_FILES'
     file_exists(filename)
+    create_folder(foldername)
 
 if __name__ == '__main__':
     main()
