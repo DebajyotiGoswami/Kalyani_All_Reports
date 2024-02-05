c1f9d7ba7e71b0ee587984e8d632b1b2308e5397
DebajyotiGoswami <goswamidiary@gmail.com>
2024-02-05 23:59:58 +0530

    tested on main file . all working fine . done for toninght . tomorrow will think about summary report of pending nsc

--- crm_playground.py
+++ crm_playground.py
@@ -188,10 +188,10 @@ def new_connection(nsc_df):
     # os.chdir(actual_path)
 
 def main():
-    filename = "APPLICATION_DETAILS_TEMP.xlsx"
+    filename = "APPLICATION_DETAILS.xlsx"
     foldername = 'ALL_CRM_FILES'
     file_exists(filename)  #check if the file exists or send error message
-    master_df = prepare_df_master(filename) #create the dataframe of the total master data
+    master_df = prepare_df_master(filename) #create the datafram of the total master data
     create_folder(foldername + "-" + str(datetime.today())[:10]) #create folder , if not exits , and cd into it
     prob_ccc_wise_file_creation(master_df) #problem wise and ccc wise master data creation
     new_connection(master_df[master_df['PROB_TYPE'] == 'New Connection']) #nsc related different reports 
