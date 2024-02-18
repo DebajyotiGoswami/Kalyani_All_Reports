5d975bb7bf91c46d853acfd6a93ad606a65e5fe7
DebajyotiGoswami <goswamidiary@gmail.com>
2024-02-18 23:50:54 +0530

    meter_playground.py --> different functions structure created . Logic not yet implemented

--- meter_playground.py
+++ meter_playground.py
@@ -1,5 +1,47 @@
 from crm_playground import file_exists , prepare_df_master , create_folder , ccc_wise_file_creation
 from datetime import datetime
+import os
+
+
+def defective_meter(df):
+    '''
+    function to create different defective meter reports like total defective , 
+    3 phase defective , 2kw defective , new defective etc
+    
+    argument -- dataframe
+    return -- None
+    '''
+    print(os.getcwd())
+
+def conventional_meter(df):
+    '''
+    function to create different conventional meter reports like total conventional , 
+    3 phase conventional , 2kw conventional etc
+    
+    argument -- dataframe
+    return -- None
+    '''
+    print(os.getcwd())
+
+def hybrid_meter(df):
+    '''
+    function to create different hybrid meter reports like total hybrid , 
+    3 phase hybrid , 2kw hybrid etc
+    
+    argument -- dataframe
+    return -- None
+    '''
+    print(os.getcwd())
+
+def other_report(df):
+    '''
+    function to create different meter reports like warranty period meters , 
+    g20_g21 meters , new installed meters etc
+    
+    argument -- dataframe
+    return -- None
+    '''
+    print(os.getcwd())
 
 def main():
     filename = "meter_status_report.xlsx"
@@ -8,7 +50,10 @@ def main():
     master_df = prepare_df_master(filename) #create the datafram of the total master data
     create_folder(foldername + "-" + str(datetime.today())[:10]) #create folder , if not exits , and cd into it
     ccc_wise_file_creation(master_df , "CCC_CODE") #problem wise and ccc wise master data creation
-    # new_connection(master_df[master_df['PROB_TYPE'] == 'New Connection']) #nsc related different reports
+    defective_meter(master_df) #different defective meter reports creation
+    conventional_meter(master_df) #different conventional meter reports creation
+    hybrid_meter(master_df) #different hybrid meter reports creation
+    other_report(master_df) #other meter reports creation
 
 if __name__ == '__main__':
     main()
