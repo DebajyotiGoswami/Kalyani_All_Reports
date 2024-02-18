70911c463e88205479d368168f0cc5f15816610d
DebajyotiGoswami <goswamidiary@gmail.com>
2024-02-18 16:07:42 +0530

    something not working . temporary commit

--- meter_playground.py
+++ meter_playground.py
@@ -1,7 +1,5 @@
-from crm_playground import file_exists , prepare_df_master , create_folder
+from crm_playground import file_exists , prepare_df_master , create_folder , ccc_wise_file_creation
 from datetime import datetime
-
-
 
 def main():
     filename = "meter_status_report.xlsx"
@@ -9,7 +7,7 @@ def main():
     file_exists(filename)  #check if the file exists or send error message
     master_df = prepare_df_master(filename) #create the datafram of the total master data
     create_folder(foldername + "-" + str(datetime.today())[:10]) #create folder , if not exits , and cd into it
-    # prob_ccc_wise_file_creation(master_df) #problem wise and ccc wise master data creation
+    ccc_wise_file_creation(master_df , 'CCC_CODE') #problem wise and ccc wise master data creation
     # new_connection(master_df[master_df['PROB_TYPE'] == 'New Connection']) #nsc related different reports
 
 if __name__ == '__main__':
