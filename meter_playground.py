a705e9fe656d5e1a5d405c6e28abb0c85c00d3a8
DebajyotiGoswami <goswamidiary@gmail.com>
2024-02-18 10:58:43 +0530

    meter_playgroun.py --> create_folder() successfully imported fromo crm_playgroun.py and used in meter_playground.py

--- meter_playground.py
+++ meter_playground.py
@@ -1,4 +1,6 @@
-from crm_playground import file_exists , prepare_df_master
+from crm_playground import file_exists , prepare_df_master , create_folder
+from datetime import datetime
+
 
 
 def main():
@@ -6,7 +8,7 @@ def main():
     foldername = 'ALL_METER_FILES'
     file_exists(filename)  #check if the file exists or send error message
     master_df = prepare_df_master(filename) #create the datafram of the total master data
-    # create_folder(foldername + "-" + str(datetime.today())[:10]) #create folder , if not exits , and cd into it
+    create_folder(foldername + "-" + str(datetime.today())[:10]) #create folder , if not exits , and cd into it
     # prob_ccc_wise_file_creation(master_df) #problem wise and ccc wise master data creation
     # new_connection(master_df[master_df['PROB_TYPE'] == 'New Connection']) #nsc related different reports
 
